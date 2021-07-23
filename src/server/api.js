679e8ad62e40889b39956e2dfa7458e4ccc08446
Chang Liu <liuc223@gmail.com>
2021-07-22 21:28:39 -0400

    Add nodemon for server code

--- src/server/api.js
+++ src/server/api.js
@@ -8,7 +8,7 @@ router.use(function timeLog(req, res, next) {
 });
 // define the home page route
 router.get("/", function(req, res) {
-  res.send("Birds home page");
+  res.send("success");
 });
 // define the about route
 router.get("/about", function(req, res) {

2df368767751f5c7c4afa81a416047f657af03ff
Chang Liu <liuc223@gmail.com>
2021-07-23 23:37:59 -0400

    Create UI skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Test"/>
+    <div>
+      <b-tabs content-class="mt-3">
+        <b-tab title="Spotify Viz" disabled></b-tab>
+        <b-tab title="First" active><HelloWorld msg="Test" /></b-tab>
+        <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+$navbar-height: 42px;
+$padding: 8px;
+
+.nav-item:first-child > a {
+  color: black !important;
+  font-family: fantasy;
+  font-weight: bold;
+}
+
+.tab-content.mt-3 {
+  margin-top: 0px !important;
+  padding: $padding;
+  min-height: calc(100vh - #{$navbar-height});
+  width: 100%;
+  > .tab-pane.active {
+    height: calc(100vh - #{$navbar-height + $padding * 2});
+    width: calc(100% - #{$padding * 2});
+    position: absolute;
+    > * {
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
